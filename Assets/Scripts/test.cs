ccb3dd4dcce0caf6f8048cc3179b3b34e485e054
Lucas A. Nielsen <lucas>
2018-04-11 16:19:19 +0200

    add vs code dependencie

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("this is a test");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
