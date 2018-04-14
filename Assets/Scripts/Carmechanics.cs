8582d8f055b0b5997b1b14cbff61756d998fc64b
munkbusiness <munkbusiness@users.noreply.github.com>
2018-04-14 13:31:15 +0200

    multiplayer

--- Assets/Scripts/Carmechanics.cs
+++ Assets/Scripts/Carmechanics.cs
@@ -6,6 +6,12 @@ public class Carmechanics : MonoBehaviour {
     HingeJoint2D hingeJoint;
     bool isAnchoring = false;
 
+    public KeyCode rightInput { get; set; }
+    public KeyCode leftInput { get; set; }
+
+    [Range(0,2)]
+    public float speed = 1;
+
 	// Use this for initialization
 	void Start () {
         hingeJoint = GetComponent<HingeJoint2D>();
@@ -14,7 +20,7 @@ public class Carmechanics : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if(Input.GetKeyDown("space")) {
+        if(Input.GetKeyDown(rightInput)) {
 
             if(!isAnchoring) {
                 //transform.RotateAround(transform.position, Vector3.forward, 1f);
@@ -26,13 +32,33 @@ public class Carmechanics : MonoBehaviour {
                 hingeJoint.enabled = false;
                 hingeJoint.anchor = new Vector2(0f, 0f);
                 isAnchoring = false;
+                GetComponent<Rigidbody2D>().angularVelocity = 0f;
             }
         }
 
+        if (Input.GetKeyDown(leftInput)) {
+
+            if (!isAnchoring) {
+                //transform.RotateAround(transform.position, Vector3.forward, 1f);
+                hingeJoint.enabled = true;
+                hingeJoint.anchor = new Vector2(-0.3f, 0);
+                isAnchoring = true;
+            }
+            else {
+                hingeJoint.enabled = false;
+                hingeJoint.anchor = new Vector2(0f, 0f);
+                isAnchoring = false;
+                GetComponent<Rigidbody2D>().angularVelocity = 0f;
+            }
+        }
+
+
     }
 
+    
+
     private void FixedUpdate() {
-        GetComponent<Rigidbody2D>().AddForce(transform.up * 2);
+        GetComponent<Rigidbody2D>().AddForce(transform.up * speed);
 
 
     }
---
+++ Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player {
+    public int id;
+    public KeyCode leftInput;
+    public KeyCode rightInput;
+    public Color color;
+}
+
+
+public class ControllerScript : MonoBehaviour {
+
+    //editor defined variables
+    public GameObject carObject;
+
+    //other
+    public Dictionary<int, Player> playerList = new Dictionary<int, Player>();
+
+	// Use this for initialization
+	void Start () {
+
+        Transform spawnPoint = GameObject.Find("SpawnPoint").transform;
+
+        Player player1 = new Player();
+        player1.id = 1;
+        player1.leftInput = KeyCode.LeftArrow;
+        player1.rightInput = KeyCode.RightArrow;
+        player1.color = Color.red;
+        playerList.Add(1, player1);
+
+        Player player2 = new Player();
+        player2.id = 2;
+        player2.leftInput = KeyCode.A;
+        player2.rightInput = KeyCode.D;
+        player2.color = Color.blue;
+        playerList.Add(2, player2);
+
+
+        foreach (KeyValuePair<int, Player> c in playerList) {
+            GameObject car = Instantiate(carObject, new Vector3(spawnPoint.position.x+c.Key, spawnPoint.position.y, 0), spawnPoint.rotation);
+            car.GetComponent<Carmechanics>().rightInput = c.Value.rightInput;
+            car.GetComponent<Carmechanics>().leftInput = c.Value.leftInput;
+            car.GetComponent<SpriteRenderer>().color = c.Value.color;
+            if(c.Key == 1) {
+                car.GetComponentInChildren<Camera>().rect = new Rect(0, 0, .5f, 1);
+            }
+            else if(c.Key == 2) {
+                car.GetComponentInChildren<Camera>().rect = new Rect(.5f, 0, .5f, 1);
+            }
+            
+        }
+
+
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
